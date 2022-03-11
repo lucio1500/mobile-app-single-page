69601c98733a14fa414aca18cbbcc7d6d1a36460
lucio1500 <60608529+lucio1500@users.noreply.github.com>
2022-03-10 22:40:13 -0300

    subida de proyecto

---
+++ main.js
@@ -0,0 +1,335 @@
+var gameInformation = 
+[
+  {
+  "Mounth": "SEPTEMBER",
+  "GameList":[
+              {
+              "Day" : 01, 
+              "Teams": "U1 and U4",
+              "Location" : "AJ Katzenmaier",
+              "Times": "9:30 a.m.",
+              "Map":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4281.467703332423!2d-87.62962089391536!3d41.8988457523852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f69da7%3A0x15e198c063fc787c!2sAJ%20Katzenmaier%20Elementary!5e0!3m2!1ses-419!2sar!4v1639001955054!5m2!1ses-419!2sar",                
+            
+                },
+                {
+              "Day" : 01, 
+              "Teams": "U3 and U2",
+              "Location" : "Greenbay",
+              "Times": "1:00 p.m.",    
+              "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7409.776422457713!2d-87.64087760241088!3d41.91398527070436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3407260c45b%3A0xb351205fae50c6f3!2sGreenbay%20Elementary!5e0!3m2!1ses-419!2sar!4v1639001989675!5m2!1ses-419!2sar",         
+             },
+              {
+                "Day" : 08,
+                "Teams": "U5 and U6",
+                "Location" : "Howard A Yeager",
+                "Times": "9:30 a.m.",
+                "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12825.041844255227!2d-87.66914378275496!3d41.92305631187245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward%20A%20Yeager%20Elementary!5e0!3m2!1ses-419!2sar!4v1639002031844!5m2!1ses-419!2sar",            
+              }
+              ,
+              {
+                "Day" : 08,
+                "Teams": "U6 and U1",
+                "Location" : "Marjorie P Hart",
+                "Times": "1:00 p.m.",    
+                "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18486.67271823722!2d-87.65374439623832!3d41.93436440538706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd30f2637f9d7%3A0xdbff5d5dfcfcfa35!2sMarjorie%20P%20Hart%20Elementary!5e0!3m2!1ses-419!2sar!4v1639002065772!5m2!1ses-419!2sar",           
+            },
+              {
+                "Day" : 15,
+                "Teams": "U2 and U4",
+                "Location" : "North",
+                "Times": "9:30 a.m.",    
+                "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d45835.290978182675!2d-87.83361405691848!3d42.35003252252837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfe6df83b0f197ed6!2sNorth%20Elementary%20School!5e0!3m2!1ses-419!2sar!4v1639002305906!5m2!1ses-419!2sar",          
+              },
+              {
+                "Day" : 15,
+                "Teams": "U3 and U5",
+                "Location" : "AJ Katzenmaier",
+                "Times": "1:00 p.m.",
+                "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4281.467703332423!2d-87.62962089391536!3d41.8988457523852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f69da7%3A0x15e198c063fc787c!2sAJ%20Katzenmaier%20Elementary!5e0!3m2!1ses-419!2sar!4v1639001955054!5m2!1ses-419!2sar",             
+            },
+            {
+                  "Day" : 22,
+                  "Teams": "U1 and U3",
+                  "Location" : "South",
+                  "Times": "9:30 a.m.",
+                  "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d45865.32231766765!2d-87.87256767555482!3d42.30883235907674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880feccac2135a21%3A0x8ce88e90b96bbec1!2sSouth%20Elementary%20School%20(K-5)!5e0!3m2!1ses-419!2sar!4v1639002129055!5m2!1ses-419!2sar",            
+            },
+            {
+                  "Day" : 22,
+                  "Teams": "U2 and U6",
+                  "Location" : "Howard A Yeager",
+                  "Times": "1:00 p.m.",
+                  "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12825.041844255227!2d-87.66914378275496!3d41.92305631187245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward%20A%20Yeager%20Elementary!5e0!3m2!1ses-419!2sar!4v1639002031844!5m2!1ses-419!2sar",           
+            },  
+            {
+                  "Day" : 29,        
+                  "Teams": "U4 and U5",
+                  "Location" : "Greenbay",
+                  "Times": "9:30 a.m.",  
+                  "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7409.776422457713!2d-87.64087760241088!3d41.91398527070436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3407260c45b%3A0xb351205fae50c6f3!2sGreenbay%20Elementary!5e0!3m2!1ses-419!2sar!4v1639001989675!5m2!1ses-419!2sar",                     
+            }
+          ]
+  },
+  { 
+  "Mounth": "OCTOBER",
+  "GameList":[
+            {
+            "Day" : 06,
+            "Teams": "U2 and U5",
+            "Location" : "Marjorie P Hart",
+            "Times": "9:30 a.m.",
+            "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18486.67271823722!2d-87.65374439623832!3d41.93436440538706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd30f2637f9d7%3A0xdbff5d5dfcfcfa35!2sMarjorie%20P%20Hart%20Elementary!5e0!3m2!1ses-419!2sar!4v1639002065772!5m2!1ses-419!2sar",
+
+            },  
+            {
+            "Day" : 06,
+            "Teams": "U1 and U6",
+            "Location" : "South",
+            "Times": "1:00 p.m.",
+            "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d45865.32231766765!2d-87.87256767555482!3d42.30883235907674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880feccac2135a21%3A0x8ce88e90b96bbec1!2sSouth%20Elementary%20School%20(K-5)!5e0!3m2!1ses-419!2sar!4v1639002129055!5m2!1ses-419!2sar",  
+
+            },
+            {
+            "Day" : 08,
+            "Teams": "U3 and U4",
+            "Location" : "Howard A Yeager",
+            "Times": "9:30 a.m.",
+            "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12825.041844255227!2d-87.66914378275496!3d41.92305631187245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward%20A%20Yeager%20Elementary!5e0!3m2!1ses-419!2sar!4v1639002031844!5m2!1ses-419!2sar",
+
+            }
+            ,
+            {
+            "Day" : 08,
+            "Teams": "U5 and U1",
+            "Location" : "Greenbay",
+            "Times": "1:00 p.m.",              
+            "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7409.776422457713!2d-87.64087760241088!3d41.91398527070436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3407260c45b%3A0xb351205fae50c6f3!2sGreenbay%20Elementary!5e0!3m2!1ses-419!2sar!4v1639001989675!5m2!1ses-419!2sar", 
+ 
+            },
+            {
+            "Day" : 20,
+            "Teams": "U6 and U3",
+            "Location" : "North",
+            "Times": "9:30 a.m.",              
+            "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d45835.290978182675!2d-87.83361405691848!3d42.35003252252837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfe6df83b0f197ed6!2sNorth%20Elementary%20School!5e0!3m2!1ses-419!2sar!4v1639002305906!5m2!1ses-419!2sar",
+
+            },
+            {
+            "Day" : 20,
+            "Teams": "U2 and U4",
+            "Location" : "Marjorie P Hart",
+            "Times": "1:00 p.m.",           
+            "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18486.67271823722!2d-87.65374439623832!3d41.93436440538706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd30f2637f9d7%3A0xdbff5d5dfcfcfa35!2sMarjorie%20P%20Hart%20Elementary!5e0!3m2!1ses-419!2sar!4v1639002065772!5m2!1ses-419!2sar",
+
+            },
+            {
+            "Day" : 27,
+            "Teams": "U3 and U1",
+            "Location" : "AJ Katzenmaier",
+            "Times": "9:30 a.m.",
+            "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4281.467703332423!2d-87.62962089391536!3d41.8988457523852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd34e07f69da7%3A0x15e198c063fc787c!2sAJ%20Katzenmaier%20Elementary!5e0!3m2!1ses-419!2sar!4v1639001955054!5m2!1ses-419!2sar",
+
+            },
+            {
+            "Day" : 27,
+            "Teams": "U5 and U6",
+            "Location" : "Howard A Yeager",
+            "Times": "1:00 p.m.",
+            "Map": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12825.041844255227!2d-87.66914378275496!3d41.92305631187245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward%20A%20Yeager%20Elementary!5e0!3m2!1ses-419!2sar!4v1639002031844!5m2!1ses-419!2sar",
+
+            }
+          ]
+  }  
+];
+
+function changeSize()
+{
+  matchMedia('(max-height: 426px) and (orientation: landscape)').matches
+  ? render(gameInformation)
+  : renderTableInfo(gameInformation)
+}
+
+addEventListener('resize',changeSize);
+addEventListener('DOMContentLoaded',changeSize);
+
+function cargaNombre(event)
+{
+  event.preventDefault();
+  
+  localStorage.clear();  
+  let nombreNav = document.getElementById("inpName").value;
+  localStorage.setItem("nombre",nombreNav);
+}
+
+function insertarNombre()
+{
+  var localName = localStorage.getItem("nombre");
+  document.getElementsByName("nombre-nav").forEach(inpName=>{
+    inpName.innerHTML = localName;
+  })
+}
+
+function load()
+{
+  document.getElementById("pagina1").style.display = "block";
+  document.getElementById("pagina2").style.display = "none";
+  document.getElementById("pagina3").style.display = "none";
+}
+
+function paginacion()
+{ 
+    const pag1 = document.getElementById("page-1");
+    const pag2 = document.getElementById("page-2");
+    const pag3 = document.getElementById("page-3");
+
+    pag1.addEventListener("click", () => {
+      document.getElementById("pagina1").style.display = "block";
+      document.getElementById("pagina2").style.display = "none";
+      document.getElementById("pagina3").style.display = "none";
+      pag1.className = "active-pagination";
+      pag3.className = pag1.className.replace( /(?:^|\s)active-pagination(?!\S)/g , '' );
+      pag2.className =pag2.className.replace( /(?:^|\s)active-pagination(?!\S)/g , '' );
+      });
+      
+      pag2.addEventListener("click", () => {
+        document.getElementById("pagina1").style.display = "none";
+        document.getElementById("pagina2").style.display = "block";
+        document.getElementById("pagina3").style.display = "none";
+      
+        pag2.className = "active-pagination";
+        pag1.className = pag1.className.replace( /(?:^|\s)active-pagination(?!\S)/g , '' );
+        pag3.className =pag2.className.replace( /(?:^|\s)active-pagination(?!\S)/g , '' );
+      });
+    
+      pag3.addEventListener("click", () => {
+        document.getElementById("pagina1").style.display = "none";
+        document.getElementById("pagina2").style.display = "none";
+        document.getElementById("pagina3").style.display = "block";
+        pag3.className = "active-pagination";
+        pag1.className = pag1.className.replace( /(?:^|\s)active-pagination(?!\S)/g , '' );
+        pag2.className =pag2.className.replace( /(?:^|\s)active-pagination(?!\S)/g , '' );
+      });
+
+}
+
+function renderTableInfo(gameInformation)
+{
+  let dayAnterior=0;
+  document.getElementById("tbody").innerHTML=``;
+  document.getElementById("tbody2").innerHTML=``;
+  gameInformation[0].GameList.forEach((data) => {
+      let tr = document.createElement("tr");   
+      if(dayAnterior!=data.Day)
+      {
+       tr.innerHTML = `<td rowspan="2">${data.Day}</td>`  
+       dayAnterior=data.Day;
+      }  
+      tr.innerHTML +=  `
+           <td>${data.Teams} <br> <a target="_blank" href=${data.Map}>${data.Location}</a></td>
+           <td>${data.Times}</td>
+           `;      
+      document.getElementById("tbody").appendChild(tr);
+    });
+    dayAnterior=0;
+    gameInformation[1].GameList.forEach((data) => {
+      let tr = document.createElement("tr");
+      if(dayAnterior!=data.Day)
+      {
+      tr.innerHTML = `
+      <td rowspan="2">${data.Day}</td>`
+      dayAnterior=data.Day;
+      }      
+      tr.innerHTML +=`
+      <td>${data.Teams} <br> <a target="_blank" href=${data.Map}>${data.Location}</a></td>
+      <td>${data.Times}</td>
+      </tr>`;
+      document.getElementById("tbody2").appendChild(tr);
+    });
+}
+
+function render(gameInformation)
+{
+  document.getElementsByName("thead").forEach(elemento =>{
+    elemento.innerHTML =`` ;
+  });
+  document.getElementById("tbody").innerHTML=``;
+  document.getElementById("tbody2").innerHTML=``;
+
+  gameInformation[0].GameList.forEach((data,index) => {
+      let tr = document.createElement("tr");
+      tr.id = "columna"+index;
+      let id = "sep"+index;
+      tr.innerHTML = `
+      <td class="col-2" id="${id}" name="septemberColum" onclick="renderInfo(gameInformation[0],${index},'contenido')">${data.Teams} </td>
+      </tr>`;
+      document.getElementById("tbody").appendChild(tr);
+    });
+    gameInformation[1].GameList.forEach((data,index) => {
+      let tr = document.createElement("tr");
+      tr.id = "columnaB"+index;
+      let id = "oct"+index;
+      tr.innerHTML = `
+      <td class="col-2" id="${id}" name="octoberColum" onclick="renderInfo(gameInformation[1],${index},'contenido2')">${data.Teams} </td>
+      </tr>`;
+      document.getElementById("tbody2").appendChild(tr);
+    });
+    let tr = document.createElement("td");
+    tr.rowSpan = 9;
+    tr.id = "contenido";
+    document.getElementById("columna0").appendChild(tr); 
+    let tr2 = document.createElement("td");
+    tr2.rowSpan = 9;
+    tr2.id = "contenido2";
+    document.getElementById("columnaB0").appendChild(tr2);
+    renderInfo(gameInformation[0],0,"contenido");
+    renderInfo(gameInformation[1],0,"contenido2");  
+}
+
+function renderInfo(obj, objClick,idRow)
+{
+  let div = document.createElement("div");
+  div.id = "contenido-game";
+  div.className = "row";
+  div.innerHTML =` 
+  <div class="col-4">
+  <p> Day: ${obj.GameList[objClick].Day} </p>
+  <p> Location: ${obj.GameList[objClick].Location} </p>
+  <p> Times: ${obj.GameList[objClick].Times} </p>
+  </div>
+  <iframe class="col-7" src=${obj.GameList[objClick].Map}></iframe>
+  `;
+
+  document.getElementById(idRow).innerHTML=``;
+  document.getElementById(idRow).append(div);
+
+  if(obj.Mounth=="SEPTEMBER")
+  {
+    document.getElementsByName("septemberColum").forEach(element => {
+      element.className = element.className.replace( /(?:^|\s)active-nav(?!\S)/g , '' );
+    });
+    document.getElementById("sep"+objClick).className="active-nav";
+  }
+
+  if(obj.Mounth=="OCTOBER")
+  {
+    document.getElementsByName("octoberColum").forEach(element => {
+      element.className = element.className.replace( /(?:^|\s)active-nav(?!\S)/g , '' );
+    });
+    document.getElementById("oct"+objClick).className="active-nav";
+  }
+}
+
+function ocultarSecciones(id)
+{ 
+  document.getElementsByName("secciones").forEach(data=>{
+
+    if(data.id!=id)
+    {
+      data.style.display="none";
+    }
+    else
+    {
+      data.style.display="block";
+    }   
+  })
+}
